f27b4b09fe92f30fa0b9943dfef0a4d064c84511
liangguanghui <liangguanghui@keep.com>
2022-02-16 14:32:27 +0800

    feature: add file

---
+++ Sources/GHFoundation/GHJSONReader.swift
@@ -0,0 +1,18 @@
+//
+//  GHJSONReader.swift
+//  
+//
+//  Created by 梁光辉 on 2022/2/12.
+//
+
+import UIKit
+
+/// 读取本地 json 文件
+class GHJSONReader: NSObject {
+
+    override init() {
+        super.init()
+    }
+    
+    
+}
