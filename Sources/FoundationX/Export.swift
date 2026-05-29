1d18a749054a7fb7f08b8d51c4fce48389d38417
Guanghui Liang <meetguanghuiliang@gmail.com>
2026-05-30 01:08:52 +0800

    ✨ FoundationX: Export Objective-C module for SPM

---
+++ Sources/FoundationX/Export.swift
@@ -0,0 +1,11 @@
+//
+//  Export.swift
+//  FoundationX 
+// 
+//  Created by 梁光辉 on 2026/5/30.
+//  Copyright © 2026 Guanghui Liang. All rights reserved.
+//
+
+#if SPM_MODE
+@_exported import FoundationX_Objc
+#endif
